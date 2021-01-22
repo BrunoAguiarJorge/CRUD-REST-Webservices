dca7a60552784d0089e8a6bdad7c37ae89c7cfe4
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-22 18:03:03 +0000

    Pagination and all CRUD working

--- src/main/java/com/brunoaguiar/crud/resourcies/ClientResource.java
+++ src/main/java/com/brunoaguiar/crud/resourcies/ClientResource.java
@@ -1,19 +1,28 @@
 package com.brunoaguiar.crud.resourcies;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.brunoaguiar.crud.entities.Client;
+import com.brunoaguiar.crud.dto.ClientDto;
 import com.brunoaguiar.crud.services.ClientService;
 
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientResource {
 
 	@Autowired
@@ -21,9 +30,44 @@ public class ClientResource {
 	
 	//responseEntity is an object that encapsulates an HTTP response.
 	@GetMapping // means this is an end point
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		return ResponseEntity.ok().body(list);
-						
+	public ResponseEntity<Page<ClientDto>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<ClientDto> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);						
 	}
+	
+	//value add id in the http: after client
+	@GetMapping(value = "/{id}")
+	//@PathVariable links {id} to parameter Long id.
+	public ResponseEntity<ClientDto> findById(@PathVariable Long id){
+		ClientDto dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDto> insert(@RequestBody ClientDto dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDto> update(@PathVariable Long id, @RequestBody ClientDto dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDto> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/brunoaguiar/crud/services/ClientService.java
+++ src/main/java/com/brunoaguiar/crud/services/ClientService.java
@@ -1,11 +1,14 @@
 package com.brunoaguiar.crud.services;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brunoaguiar.crud.dto.ClientDto;
 import com.brunoaguiar.crud.entities.Client;
 import com.brunoaguiar.crud.repositories.ClientRepository;
 
@@ -16,8 +19,48 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public Page<ClientDto> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDto(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDto findById(Long id){
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDto(entity);
+		
+	}
+	@Transactional
+	public ClientDto insert(ClientDto dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDto(entity);		
+	
+	}
+	
+	@Transactional
+	public ClientDto update(Long id, ClientDto dto) {
+		Client entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDto(entity);	
+	}
+
+	
+	public ClientDto delete(Long id) {
+		repository.deleteById(id);
+		return new ClientDto();
+		
 	}
 	
 }
